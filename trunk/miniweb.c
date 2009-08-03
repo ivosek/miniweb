8e12edf34820cff3c86f25d24c67f785f4a7f93c
stanleyhuangyc <stanleyhuangyc@4ed72ee2-fddc-11dd-984d-ab748d24aa7e>
2009-08-03 08:37:05 +0000

    fix open() argument issue under Linux

--- trunk/miniweb.c
+++ trunk/miniweb.c
@@ -99,7 +99,7 @@ int DefaultWebFileUploadCallback(HttpMultipart *pxMP, OCTET *poData, size_t dwDa
 	if (!fd) {
 		char filename[256];
 		snprintf(filename, sizeof(filename), "%s/%s", httpParam[0].pchWebPath, pxMP->pchFilename);
-		fd = open(filename, O_CREAT | O_TRUNC | O_RDWR | O_BINARY);
+		fd = open(filename, O_CREAT | O_TRUNC | O_RDWR | O_BINARY, 0);
 		pxMP->pxCallBackData = (void*)fd;
 	}
 	if (fd <= 0) return -1;
